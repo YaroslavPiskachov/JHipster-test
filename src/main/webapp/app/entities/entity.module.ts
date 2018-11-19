c54a1460e668f2644504dfbfc58a013fdadb7a23
Yaroslav P <yaroslav.p@implemica.com>
2018-11-19 23:39:56 +0200

    first commit

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,10 +1,26 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { JhipsterTestRegionModule } from './region/region.module';
+import { JhipsterTestCountryModule } from './country/country.module';
+import { JhipsterTestLocationModule } from './location/location.module';
+import { JhipsterTestDepartmentModule } from './department/department.module';
+import { JhipsterTestTaskModule } from './task/task.module';
+import { JhipsterTestEmployeeModule } from './employee/employee.module';
+import { JhipsterTestJobModule } from './job/job.module';
+import { JhipsterTestJobHistoryModule } from './job-history/job-history.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
+        JhipsterTestRegionModule,
+        JhipsterTestCountryModule,
+        JhipsterTestLocationModule,
+        JhipsterTestDepartmentModule,
+        JhipsterTestTaskModule,
+        JhipsterTestEmployeeModule,
+        JhipsterTestJobModule,
+        JhipsterTestJobHistoryModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
